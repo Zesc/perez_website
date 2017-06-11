40d672ce8581d8cd87d0e037355893a442c41eef
elvynmejia <elvynmejia@gmail.com>
2017-06-11 11:50:47 -0700

    Updates with new content

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-helo
+
+//do this on document ready
+//with jQuery yuu con do it like this: $(document).ready(function(){ //query DOM manipulation});
+var homePage = document.querySelector('#home-page');
+console.log('home page => ', homePage);
+
+if (homePage != null) {
+	//get navbar by id or class, remove from DOM
+	//query home page image
+	//append navbar after image
+	//happy coding! 
+}
